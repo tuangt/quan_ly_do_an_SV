89363086f01946d8503566ff5ee8776e43840901
Phan Anh Tuấn <tuancatter@gmail.com>
2017-04-19 15:55:32 +0700

    Tạo Controller trắng

---
+++ QLDA_MVC_UI/Controllers/TuanAnhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDA_MVC_UI.Controllers
+{
+    public class TuanAnhController : Controller
+    {
+        // GET: TuanAnh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
